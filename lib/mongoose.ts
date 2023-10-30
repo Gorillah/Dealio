14657e6b059c67f233fec3bbd7a4b37c60b72663
Bilal <jcvntx@gmail.com>
2023-10-30 23:41:36 +0300

    maxDuration vercel route for cron

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,7 +4,6 @@ let isConnected = false; // Variable to track connection status
 
 export const connectDB = async () => {
   mongoose.set("strictQuery", true);
-  // mongoose.set("bufferTimeoutMS", 30000);
 
   if (!process.env.MONGODB_URI) return console.log("MongoDB URI not found");
 
