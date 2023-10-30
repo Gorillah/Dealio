72e4dd877dddc6f6bec32fa4aad4c2c53d56f564
Bilal <jcvntx@gmail.com>
2023-10-30 23:46:30 +0300

    -u -stable fix 1.1

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,19 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-let isConnected = false; // Variable to track connection status
+let isConnected = false;// Variable to track the connection status
 
-export const connectDB = async () => {
-  mongoose.set("strictQuery", true);
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
 
-  if (!process.env.MONGODB_URI) return console.log("MongoDB URI not found");
+  if(!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
 
-  if (isConnected) return console.log("using existing connection");
+  if(isConnected) return console.log('=> using existing database connection');
 
   try {
     await mongoose.connect(process.env.MONGODB_URI);
+
     isConnected = true;
-    console.log("Connected to db");
+
+    console.log('MongoDB Connected');
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
